3bb04fbdeeada3df8b5e804778338d6da5f86d8e
joan-gm <joannamojeco@gmail.com>
2024-07-06 15:47:26 +0800

    added images and updated code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,7 @@
-const div = document.querySelector("div")
-const spanHi = document.querySelector("#hi")
-const spanBye = document.querySelector("#bye")
-
-/*
-spanBye.remove()
-div.append(spanBye)
-
-console.log(spanHi.getAttribute("title"))
-spanHi.setAttribute("id", "change-id")
-spanHi.removeAttribute("id")
-
-console.log(spanHi.dataset)
-spanHi.dataset.newName = "new name data"
-*/
-
-spanHi.classList.add("new-class")
-spanHi.classList.remove("hi1")
-spanHi.classList.toggle("hi1")
-
-spanHi.style.color = "red"
-spanHi.style.backgroundColor = "red"
+const button =
+            document.querySelector('button');
+        button.addEventListener('click',
+            () => {
+                button.style.backgroundColor = 
+                    "green";
+            });
